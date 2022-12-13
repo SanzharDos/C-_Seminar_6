a5df9092330006f24b259c19e481e395a3c9dece
Sanzhar <dos.0227@yandex.ru>
2022-12-13 10:49:47 +0600

    Task3 done

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 42. Напишите программу, которая будет преобразовывать десятичное число в двоичное
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+try
+{
+    Console.WriteLine("Введите число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    // PrintArray(CreateArray(num));
+    string res = "";
+    while (num > 0)
+    {
+        res = Convert.ToString(value: num % 2) + res;
+        num = num / 2;
+    }
+    Console.Write($"{res}");
+}
+
+catch
+{
+    Console.WriteLine("Надо ввести целое число!!!");
+}
